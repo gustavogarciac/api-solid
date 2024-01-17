10a01b869908cc69090c080cfe8d606eb2c1dd52
gustavo <gustavogarciaci1@gmail.com>
2024-01-16 21:18:25 -0300

    feat: generating new database url for each e2e test

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,41 @@
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { execSync } from "node:child_process";
+
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error(
+      "[ENV ERROR] Please provide a DATABASE_URL environment variable"
+    );
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   async setup() {
-    console.log("Executou");
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseURL; // Changing the database url to the one with the schema in order to have different databases for each test
+
+    execSync(`npx prisma migrate deploy`); // Execute prisma migrations */
 
     return {
-      teardown() {
-        console.log("Teardown");
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+        await prisma.$disconnect();
       },
     };
   },
